e846b0349f93fe926dd54c41935aecdea9d6fda2
DESKTOP-QKDP7OI\manhh <manhhieu012@gmail.com>
2023-12-24 08:39:37 +0700

    kinchana

---
+++ Demo/ImguiIntegrationDemo.cs
@@ -0,0 +1,82 @@
+﻿using CruZ.UI;
+using ImGuiNET;
+using Microsoft.Xna.Framework;
+using Num = System.Numerics;
+
+
+namespace CruZ.Demos
+{
+    [DemoName("ImguiIntegrationDemo")]
+    internal class ImguiIntegrationDemo : CruZ_App
+    {
+        public ImguiIntegrationDemo()
+        {
+            Core.Run();
+        }
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            _imgui = new(Core);
+            _imgui.RebuildFontAtlas();
+
+            Core.OnDraw += Draw;
+        }
+
+        float a = 0;
+
+        private void Draw(GameTime gameTime)
+        {
+            _imgui.BeforeLayout(gameTime);
+
+            // Draw our UI
+            {
+                ImGui.Begin("test",
+                    ImGuiWindowFlags.NoTitleBar);
+
+                var io = ImGui.GetIO();
+
+                ImGui.Text(io.MousePos.ToString());
+                ImGui.Text(a.ToString());
+
+                ImGui.PushItemWidth(50);
+                ImGui.SliderFloat("Slider", ref a, 0.0f, 1.0f);
+                ImGui.PopItemWidth();
+
+                ImGui.End();
+            }
+
+            // Call AfterLayout now to finish up and draw all the things
+            _imgui.AfterLayout();
+        }
+
+        ImGuiRenderer _imgui;
+
+        private float f = 0.0f;
+
+        private bool show_test_window = false;
+        private bool show_another_window = false;
+        private Num.Vector3 clear_color = new Num.Vector3(114f / 255f, 144f / 255f, 154f / 255f);
+        private byte[] _textBuffer = new byte[100];
+
+
+
+        unsafe float[] _cameraView = new float[16]
+           { 1.0f, 0.0f, 0.0f, 0.0f,
+             0.0f, 1.0f, 0.0f, 0.0f,
+             0.0f, 0.0f, 1.0f, 0.0f,
+             0.0f, 0.0f, 0.0f, 1.0f };
+
+        unsafe float[] _projection = new float[16]
+           { 1.0f, 0.0f, 0.0f, 0.0f,
+             0.0f, 1.0f, 0.0f, 0.0f,
+             0.0f, 0.0f, 1.0f, 0.0f,
+             0.0f, 0.0f, 0.0f, 1.0f };
+        
+        unsafe float[] _identity = new float[16]
+           { 1.0f, 0.0f, 0.0f, 0.0f,
+             0.0f, 1.0f, 0.0f, 0.0f,
+             0.0f, 0.0f, 1.0f, 0.0f,
+             0.0f, 0.0f, 0.0f, 1.0f };
+    }
+}
---
+++ src/CruZ_Engine/Core/CruZ_Singleton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CruZ
+{
+    public partial class CruZ
+    {
+        private static CruZ _instance;
+        public static CruZ Instance()
+        {
+            if (_instance == null)
+                _instance = new CruZ();
+            return _instance;
+        }
+
+        public static readonly int VIRTUAL_WIDTH = 1080;
+        public static readonly int VIRTUAL_HEIGHT = 1920;
+        public static readonly string CONTENT_ROOT = "Content";
+    }
+}
